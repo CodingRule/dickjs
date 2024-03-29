094600319831a131018dd13ebe375b3be7f7be0b
CodingRule <91148727+CodingRule@users.noreply.github.com>
2024-03-29 17:18:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const asciify = require('asciify-image');
 
-async function generateThingy() {
-    const imageUrl = "https://static-00.iconduck.com/assets.00/penis-emoji-2048x2004-mcysxmme.png";
-    const options = {
-        fit: 'box',
-        width: 20, 
-        height: 20, 
-        color: false 
-    };
+async function generateThingy(imageUrl) {
+    const options = { fit: 'box', width: 20, height: 20, color: false };
 
-    return new Promise((resolve, reject) => {
-        asciify(imageUrl, options, (err, asciified) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(asciified);
-            }
+    try {
+        return await new Promise((resolve, reject) => {
+            asciify(imageUrl, options, (err, asciified) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(asciified);
+                }
+            });
         });
-    });
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = generateThingy;
